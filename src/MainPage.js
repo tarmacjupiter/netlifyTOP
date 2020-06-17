7dc9df4079ac4751e8245d12947f4c20b908549b
tarmacjupiter <beheryanthony@gmail.com>
2020-06-17 18:49:21 -0500

    Google Slide Player Test

--- src/MainPage.js
+++ src/MainPage.js
@@ -4,6 +4,7 @@ import Title from "./Title";
 import Card from "./Card";
 import Slides from "./Slides";
 import { Button } from "react-bootstrap";
+import ReactGoogleSlides from "react-google-slides";
 
 function newFormTab() {
   window.open(
@@ -28,6 +29,13 @@ export default function MainPage() {
           Bugs
         </Button>
       </div>
+      <ReactGoogleSlides
+        width={"600rem"}
+        height={"400rem"}
+        slidesLink="https://docs.google.com/presentation/d/1l1Y8SnH0CcTz7JHilU4hk8yXyjUHCvbDJ5tv3beOPSE/edit?usp=sharing"
+        showControls
+        slideDuration={999999}
+      />
     </div>
   );
 }
